8cf00fb4efec8ef38b5f38e37e8ff12857eb3266
Gary Smith <garysmith193358@gmail.com>
2023-12-08 17:54:10 -0500

    add solution for tower breakers

---
+++ Day3/tower_breakers.py
@@ -0,0 +1,24 @@
+
+def towerBreakers(n, m):
+    if n % 2 == 0 or m == 1:
+        return 2
+    
+    return 1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        m = int(first_multiple_input[1])
+
+        result = towerBreakers(n, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
